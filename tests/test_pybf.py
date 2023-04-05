4010bb2771c7909c01a879b7b86fb074acf5af54
lbreede <lennart.breede@gmail.com>
2023-04-04 17:30:43 -0700

    Initial commit

--- tests/test_pybf.py
+++ tests/test_pybf.py
@@ -10,9 +10,6 @@ class TestPyBf(unittest.TestCase):
         cls.bf = Brainfuck()
         cls.bf_files = os.path.join(os.path.dirname(__file__), "..", "bf_files")
 
-    def tearDown(self) -> None:
-        self.bf.reset()
-
     def test_files(self) -> None:
         path = os.path.join(self.bf_files, "hello_world.bf")
         self.bf.load(path)
@@ -25,3 +22,48 @@ class TestPyBf(unittest.TestCase):
         output = self.bf.run()
         self.assertEqual(output, "Hello, World!")
         self.assertEqual(self.bf.output, "Hello, World!")
+
+    def test_two_programs(self) -> None:
+        self.bf.program = (
+            "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++."
+        )
+        output = self.bf.run()
+        self.assertEqual(output, "Hello")
+        self.assertEqual(self.bf.output, "Hello")
+
+        self.bf.reset()
+
+        self.bf.program = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>++++++++++++++.------------.>>-------------.++++++++++++++++++++++++.+++.------.--------.<<+."
+        output = self.bf.run()
+        self.assertEqual(output, "Hello, World!")
+        self.assertEqual(self.bf.output, "Hello, World!")
+
+    def test_partial_program(self) -> None:
+        program = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+."
+        program_half_length = len(program) // 2
+        program_a = program[:program_half_length]
+        program_b = program[program_half_length:]
+        self.bf.program = program_a
+        self.bf.run()
+        self.bf.program = program_b
+        self.bf.instruction_pointer = 0
+        output = self.bf.run()
+        self.assertEqual(output, "Hello, World!")
+        self.assertEqual(self.bf.output, "Hello, World!")
+
+    def test_reset(self) -> None:
+        """Test reset method with and without including output"""
+        self.bf.program = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+."
+        self.bf.run()
+        self.bf.reset()
+
+        self.assertEqual(self.bf.data_pointer, 0)
+        self.assertEqual(self.bf.instruction_pointer, 0)
+        self.assertNotEqual(self.bf.output, "")
+
+        self.bf.reset(include_output=True)
+
+        self.assertEqual(self.bf.output, "")
+
+    def tearDown(self) -> None:
+        self.bf.reset(include_output=True)
